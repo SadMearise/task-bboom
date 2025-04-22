d129bd7257fd0bc02fff3d997902bffac3f95ee4
Nikita Khrustalev <nikita.khrustalev2012@gmail.com>
2025-04-22 15:35:34 +0300

    fix: change interface to type

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -2,10 +2,10 @@ import { FC, PropsWithChildren } from "react";
 import { LINKS } from "../utils/constants";
 import { Navigate, Outlet } from "react-router";
 
-interface Props {
+type Props = {
   isAllowed: boolean;
   redirectPath?: string;
-}
+};
 
 const ProtectedRoute: FC<PropsWithChildren<Props>> = ({
   isAllowed,
