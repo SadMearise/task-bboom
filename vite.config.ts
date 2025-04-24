d80c454cf28b3a1d2a2a1e7339fc37c4e8cfe6cc
Nikita Khrustalev <nikita.khrustalev2012@gmail.com>
2025-04-24 15:37:07 +0300

    chore: change vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ command }) => {
   const config = {
     plugins: [react()],
-    base: command === "build" ? "/task-bboom/" : "/",
+    base: command === "build" ? "/task-bboom/dist" : "/",
   };
 
   return config;
